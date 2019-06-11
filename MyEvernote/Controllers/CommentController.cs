439003fbaca446b88f4db784b4db8788dc37e49b
CoskunKarakoc <coskunkarakocc@gmail.com>
2019-06-11 21:33:41 +0300

    Comment_Process

---
+++ MyEvernote/Controllers/CommentController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MyEvernote.BusinessLayer;
+using MyEvernote.Entities;
+using MyEvernote.Models;
+
+namespace MyEvernote.Controllers
+{
+    public class CommentController : Controller
+    {
+        private NoteManager _noteManager = new NoteManager();
+        private CommentManager _commentManager = new CommentManager();
+        public ActionResult ShowNoteComments(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            //Note note = _noteManager.Find(x=>x.Id==id.Value);
+            Note notee = _noteManager.ListQueryable().Include("Comments").FirstOrDefault(x => x.Id == id.Value);
+            if (notee == null)
+            {
+                return HttpNotFound();
+            }
+
+            return PartialView("_PartialComments", notee.Comments);
+        }
+        [HttpPost]
+        public ActionResult Edit(int? id, string text)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Comment comment = _commentManager.Find(x => x.Id == id.Value);
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+
+            comment.Text = text;
+            if (_commentManager.Update(comment) > 0)
+            {
+                return Json(new { result = true }, JsonRequestBehavior.AllowGet);
+            }
+
+            return Json(new { result = false }, JsonRequestBehavior.AllowGet);
+
+
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Comment comment = _commentManager.Find(x => x.Id == id.Value);
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+
+            if (_commentManager.Delete(comment) > 0)
+            {
+                return Json(new { result = true }, JsonRequestBehavior.AllowGet);
+            }
+
+            return Json(new { result = false }, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public ActionResult Create(Comment comment, int? noteid)
+        {
+            ModelState.Remove("CreatedOn");
+            ModelState.Remove("ModifiedOn");
+            ModelState.Remove("ModifiedUsername");
+
+            if (ModelState.IsValid)
+            {
+                if (noteid == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+
+                Note note = _noteManager.Find(x => x.Id == noteid);
+
+                if (note == null)
+                {
+                    return new HttpNotFoundResult();
+                }
+
+                comment.Note = note;
+                comment.Owner = CurrentSession.User;
+
+                if (_commentManager.Insert(comment) > 0)
+                {
+                    return Json(new { result = true }, JsonRequestBehavior.AllowGet);
+                }
+            }
+
+            return Json(new { result = false }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
